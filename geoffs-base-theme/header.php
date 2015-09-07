70fd492c0c738c6d1b100d9358791776d2bd987a
mpdeegantafe <michael.deegan6@tafensw.net.au>
2015-09-07 19:21:27 +1000

    Updated header.php.

--- geoffs-base-theme/header.php
+++ geoffs-base-theme/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Chris Was Here <?php bloginfo('name'); ?>  <?php wp_title(''); ?></title>
+<title>Chris Was Here... so was Mike <?php bloginfo('name'); ?>  <?php wp_title(''); ?></title>
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 
 <?php wp_head(); ?>
